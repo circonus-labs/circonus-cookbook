24f4ea8912998cc1ab679e47b09fd7489489675d
Clinton Wolfe <clinton@omniti.com>
2013-08-13 02:26:26 +0000

    version bump to 0.0.9

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sa@omniti.com"
 license          "All rights reserved"
 description      "Circonus API client lib, resources, and such"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.9"
 name             "circonus"
