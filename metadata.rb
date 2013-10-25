e67c50ee21f05591e043fa23e91be59ca3e5dc31
Clinton Wolfe <clintonc.wolfe@zenimaxonline.com>
2013-10-25 17:26:26 -0400

    bump to v0.0.11

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "sa@omniti.com"
 license          "All rights reserved"
 description      "Circonus API client lib, resources, and such"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.10"
+version          "0.0.11"
 name             "circonus"
